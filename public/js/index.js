a0c3596f9f7c31afcc6cc5950025220b3a459753
jeremias ramirez <jereramirez499@gmail.com>
2019-04-25 20:43:46 -0400

    agregando funcionalidad de ir arriba desde titulo de pagina

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,9 @@
- 
+ let formMedia = document.getElementById("form-media");
+	formMedia.addEventListener("click", (e)=>{
+		e.preventDefault();
+	}, false)
+
+
 
 // request to tvmaze.com
 let container = document.getElementById("container");
@@ -6,12 +11,15 @@ let btnSend = document.getElementById("btn-send-js");
 let findMedia = document.getElementById("find-media");
 let titlePage = document.getElementById("title-page-js");
 
+
 	btnSend.addEventListener("click", ()=>{
+			 btnSend.classList.toggle("scale");
+		 
 			 
 
 
 		if(findMedia.value != ""){
-		btnSend.classList.add("scale");
+		
 		// container.textContent = "actualizando.."
 		// container.innerHTML = ""
 		let url = 'http://api.tvmaze.com/singlesearch/shows?q='+ findMedia.value; 
@@ -29,8 +37,8 @@ let titlePage = document.getElementById("title-page-js");
 	xhr.addEventListener("load", (e)=>{
 
 		let text = JSON.parse(e.target.responseText)
-		 	
-		 	console.log(text)
+		 	 
+		 
 		 
  			let nameMedia = document.getElementById("item-name");
  			
@@ -84,6 +92,10 @@ let titlePage = document.getElementById("title-page-js");
  				premier.innerHTML = ""
  				premier.innerHTML = text.premiered
 
+ 				let titleDocument = document.getElementById("title-document");
+ 					titleDocument.innerHTML = ""
+ 					titleDocument.innerHTML = text.name
+
  			
  
 			 
@@ -96,44 +108,21 @@ let titlePage = document.getElementById("title-page-js");
 		}
 }, false);
 
+let mainHeader = document.getElementById("main__header-js");
+
+let scrollingTop = function(scrolling){
+
+	scrolling.addEventListener("click", ()=> {
+
+		if(window.scrollY >= 1){
+			window.scrollTo(0, 0);
+		}
+
+	}, false);
+
+}
+scrollingTop(mainHeader);
 
 
 
-
-
-// Array(3) [ "Drama", "Romance", "Supernatural" ]
-// ​
-// id: 63
-// ​
-// image: Object { medium: "http://static.tvmaze.com/uploads/images/medium_portrait/88/220908.jpg", original: "http://static.tvmaze.com/uploads/images/original_untouched/88/220908.jpg" }
-// ​
-// language: "English"
-// ​
-// name: "The Vampire Diaries"
-// ​
-// network: Object { id: 5, name: "The CW", country: {…} }
-// ​
-// officialSite: "http://www.cwtv.com/shows/the-vampire-diaries"
-// ​
-// premiered: "2009-09-10"
-// ​
-// rating: Object { average: 8.1 }
-// ​
-// runtime: 60
-// ​
-// schedule: Object { time: "20:00", days: (1) […] }
-// ​
-// status: "Ended"
-// ​
-// summary: "<p><b>The Vampire Diaries</b> is a supernatural drama/romance based around Elena Gilbert who falls in love with a 163 year old vampire.</p><p>Based on a novel series by L.J. Smith.</p>"
-// ​
-// type: "Scripted"
-// ​
-// updated: 1549195605
-// ​
-// url: "http://www.tvmaze.com/shows/63/the-vampire-diaries"
-// ​
-// webChannel: null
-// ​
-// weight: 94
-// ​
+ 
